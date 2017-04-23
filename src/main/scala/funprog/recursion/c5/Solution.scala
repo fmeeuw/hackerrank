4cd66b7588ad599bf97fbe7cdbdb5281121c2043
fmeeuw <fvanmeeuwen@stackstate.com>
2017-04-23 17:07:15 +0200

    Added solution to string mingling

---
+++ src/main/scala/funprog/recursion/c5/Solution.scala
@@ -0,0 +1,24 @@
+package funprog.recursion.c5
+
+import scala.annotation.tailrec
+import scala.io.StdIn
+
+/**
+  * String Mingling
+  * https://www.hackerrank.com/challenges/string-mingling
+  */
+object Solution {
+
+  def main(args: Array[String]) {
+    val first = StdIn.readLine()
+    val second = StdIn.readLine()
+    println(mingle(first.toList, second.toList).reverse.mkString)
+  }
+
+  @tailrec
+  def mingle(restA: List[Char], restB: List[Char], acc: List[Char] = List.empty): List[Char] = {
+    if(restA.isEmpty || restB.isEmpty) acc
+    else { mingle(restA.tail, restB.tail, restB.head :: restA.head :: acc) }
+  }
+}
+
