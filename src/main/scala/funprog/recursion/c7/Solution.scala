955367a83c4a89b8c998d77fbd7b3fd0e8c10e3d
fmeeuw <fvanmeeuwen@stackstate.com>
2017-04-25 21:47:40 +0200

    Finished solution for recursive trees.

--- src/main/scala/funprog/recursion/c7/Solution.scala
+++ src/main/scala/funprog/recursion/c7/Solution.scala
@@ -12,57 +12,68 @@ import scala.io.StdIn
   */
 object Solution {
 
-  sealed trait YShapes {
+  val Present = '1'
+  val NotPresent = '_'
+
+  sealed trait YShapesList {
     /**
       * Represents this collection of YShapes in a 2-dimensional array.
-      * A '1' represents the presence of this shape, the '_' the absence.
+      * A Present represents the presence of this shape, the '_' the absence.
       */
     def toLines: Vector[Vector[Char]]
   }
+  case object Empty extends YShapesList {
+    override def toLines: Vector[Vector[Char]] = Vector.empty
+  }
   case object YShapes {
     def apply(size: Int, depth: Int): YShapes = {
-      if (depth <= 1) YShape(size)
-      else ConnectedShapes(
-        head = YShape(size),
-        left = apply(size/2, depth-1),
-        right = apply(size/2, depth-1))
+      if (depth <= 1) YShapes(YShape(size), Empty, Empty)
+      else YShapes(YShape(size), apply(size/2, depth-1), apply(size/2, depth-1))
     }
   }
-  case class ConnectedShapes(head: YShape, left: YShapes, right: YShapes) extends YShapes {
+  case class YShapes(head: YShape, left: YShapesList, right: YShapesList) extends YShapesList {
     def toLines: Vector[Vector[Char]] = {
-      head.toLines ++
+      head.toLines(head.width*2-2) ++
         left.toLines.zip(right.toLines)
-          .map { case (a,b) => a ++ Vector.fill(head.width/2)('_') ++ b }
+          .map { case (a,b) => a ++ b }
     }
   }
-  case class YShape(length: Int) extends YShapes {
+  case class YShape(length: Int) {
 
     val width = length+1 // (length of slanting segment * 2) + 1
 
-    def toLines(): Vector[Vector[Char]] = {
+    def toLines(totalSize: Int): Vector[Vector[Char]] = {
       val centerX = width/2
-      val verticalLine = Vector.fill(width)('_').updated(centerX, '1')
+      val verticalLine = Vector.fill(width)(NotPresent).updated(centerX, Present)
       val verticalSegment = (0 until (length / 2)).toVector.map(_ => verticalLine)
       val slantingSegment = (0 until (length / 2)).toVector.map { rowId =>
-        Vector.fill(width)('-')
-          .updated(centerX - (rowId + 1), '1')
-          .updated(centerX + (rowId + 1), '1')
+        Vector.fill(width)(NotPresent)
+          .updated(centerX - (rowId + 1), Present)
+          .updated(centerX + (rowId + 1), Present)
       }
-      verticalSegment ++ slantingSegment
+      (verticalSegment ++ slantingSegment)
+        .map(surroundWith(totalSize,NotPresent))
     }
   }
 
   def surroundWith[A](totalSize: Int, element: A)(list: Vector[A]): Vector[A] = {
     val remainder = totalSize - list.size
-    val right = remainder/2
-    Vector.fill(remainder-right)(element) ++ list ++ Vector.fill(right)(element)
+    val left = remainder/2
+    Vector.fill(left)(element) ++ list ++ Vector.fill(remainder-left)(element)
+  }
+
+  def ensureSize[A](totalSize: Int, fill: A)(list: Vector[A]): Vector[A] = {
+    val firstList = list.take(totalSize)
+    firstList ++ Vector.fill(totalSize - firstList.size)(fill)
   }
 
   def main(args: Array[String]): Unit = {
     val depth = StdIn.readInt()
-    YShapes(16, depth)
+    val lines: Vector[Vector[Char]] = YShapes(32, depth)
       .toLines
-      .map(surroundWith(100, '_'))
+      .map(surroundWith(100, NotPresent))
+
+    ensureSize(63, Vector.fill(100)(NotPresent))(lines)
       .reverseIterator
       .foreach { line =>
         println(line.mkString)
