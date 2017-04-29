21530ed5e7c2a0ceec5745bcdedc441fd6b5e24c
fmeeuw <fvanmeeuwen@stackstate.com>
2017-04-29 11:52:16 +0200

    Added solution for string compression

---
+++ src/main/scala/funprog/recursion/c9/Solution.scala
@@ -0,0 +1,44 @@
+package funprog.recursion.c9
+
+import scala.annotation.tailrec
+import scala.io.StdIn
+
+/**
+  * String Compression
+  * https://www.hackerrank.com/challenges/string-compression
+  *
+  * @author Frank van Meeuwen
+  */
+object Solution {
+
+  def main(args: Array[String]): Unit = {
+    val message = StdIn.readLine()
+    println(encode(message.toList))
+  }
+
+  case class CharFrequency(char: Char, frequency: Int) {
+    def plusOne = copy(frequency = frequency + 1)
+
+    override lazy val toString: String = if (frequency <= 1) s"$char" else s"$char$frequency"
+  }
+
+  def encode(message: List[Char]): String = {
+    @tailrec
+    def encodeRecursively(message: List[Char], previous: CharFrequency, encoded: List[CharFrequency]): List[CharFrequency] = {
+      message match {
+        case Nil => previous :: encoded
+        case x :: xs if (x == previous.char) => encodeRecursively(xs, previous.plusOne, encoded)
+        case x :: xs if (x != previous.char) => encodeRecursively(xs, CharFrequency(x, 1), previous :: encoded)
+      }
+    }
+
+    val charFrequencies: Seq[Solution.CharFrequency] = message match {
+      case Nil => Nil
+      case x :: xs => encodeRecursively(xs, CharFrequency(x, 1), Nil)
+    }
+
+    charFrequencies.reverseIterator.map(_.toString).mkString
+  }
+
+
+}
