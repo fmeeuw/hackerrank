4f5fabaedb9f38e425580ed86c44d1f5537f85d7
fmeeuw <fvanmeeuwen@stackstate.com>
2017-04-29 09:16:48 +0200

    Changed solution to not have Lines, but only LineSegments and adjusted the perpendicular distance method for that, such that it works for horizontal and vertical lines as well.

--- src/main/scala/funprog/recursion/c8/Solution.scala
+++ src/main/scala/funprog/recursion/c8/Solution.scala
@@ -6,8 +6,7 @@ import scala.io.StdIn
   * Convex Hull
   * https://www.hackerrank.com/challenges/convex-hull-fp
   *
-  * Implemented own algorithm for finding convex hull.
-  *
+  * Implemented own algorithm for finding convex hull, which turned out to be similar to quickHull.
   * @author Frank van Meeuwen
   */
 object Solution {
@@ -15,9 +14,13 @@ object Solution {
   object Point {
     def apply(x: Int, y: Int): Point = IPoint(x,y)
     def apply(x: Double, y: Double): Point = DPoint(x,y)
+    def readDoublesFromLine(line: String): Point = {
+      val x::y::Nil = line.split(" ").map(_.toDouble).toList
+      Point(x,y)
+    }
     def readIntsFromLine(line: String): Point = {
       val x::y::Nil = line.split(" ").map(_.toInt).toList
-      IPoint(x,y)
+      Point(x,y)
     }
     def distance(a: Point, b: Point): Double = {
       Math.sqrt(Math.pow(b.y - a.y,2) + Math.pow(b.x - a.x,2))
@@ -34,99 +37,57 @@ object Solution {
     override val y = yInt.toDouble
   }
   case class LineSegment(from: Point, to: Point) {
-    def toLine: Line = {
-      val coefficient = (to.y - from.y) / (to.x - from.x)
-      val offset = from.y - (coefficient * from.x)
-      Line(offset, coefficient)
-    }
-    lazy val length: Double = from.distance(to)
-    lazy val center: Point = Point(to.x - from.x, to.y - from.y)
-  }
-  case class Line(offset: Double, coefficient: Double) {
-    def getY(x: Double): Double = offset + (x * coefficient)
-    def getX(y: Double): Double = (y - offset) / coefficient
+    lazy val length = from.distance(to)
 
     def perpendicularDistance(point: Point): Double = {
-      val a = Point(point.x, getY(point.x))
-      val b = Point(getX(point.y), point.y)
-      LineSegment(a,b).center.distance(point)
+      val areaTriangle = Math.abs((to.y - from.y) * point.x - (to.x - from.x) * point.y + to.x * from.y - to.y * from.x)
+      areaTriangle / length
     }
 
-    def isAbove(epsilon: Double)(point: Point): Boolean = {
-      point.y > getY(point.x) + epsilon
-    }
-    def isBelow(epsilon: Double)(point: Point): Boolean = {
-      point.y < getY(point.x) - epsilon
-    }
-  }
-  case object Extremes {
-    def fromPoints(points: List[Point]): Option[Extremes] = {
-      if (points.isEmpty) None
-      else {
-        val x::xs = points
-        Some(xs.foldLeft(Extremes(x, x, x, x)){ _ updateWith _ })
-      }
-    }
-  }
-  case class Extremes(minX: Point, maxX: Point, minY: Point, maxY: Point) {
-    def updateWith(point: Point): Extremes = {
-      copy(
-        minX = if(point.x < minX.x) point else minX,
-        maxX = if(point.x > maxX.x) point else maxX,
-        minY = if(point.y < minY.y) point else minY,
-        maxY = if(point.y > maxY.y) point else maxY
-      )
+    def isAbove(point: Point): Boolean = {
+      whichSide(point) > 0
     }
 
-    lazy val distinctPoints = Set(minX,maxX,minY,maxY).toList
+    def isBelow(point: Point): Boolean = {
+      whichSide(point) < 0
+    }
+
+    private def whichSide(point: Point): Double = {
+      (to.x - from.x) * (point.y - from.y) - (point.x - from.x) * (to.y - from.y)
+    }
   }
-  case object LinePartitioning {
-    val Empty = LinePartitioning(Nil,Nil,Nil)
-  }
-  case class LinePartitioning(above: List[Point], on: List[Point], below: List[Point])
 
   def main(args: Array[String]): Unit = {
     val numberOfPoints = StdIn.readInt
     val points = (0 until numberOfPoints).map(_ => Point.readIntsFromLine(StdIn.readLine)).toList
-
-    def expandIfPossible(from: Point, to: Point, points: List[Point], aboveLine: Boolean): List[LineSegment] = {
-      if(from == to) Nil
-      else expandConvexHull(LineSegment(from,to), points, aboveLine)
-    }
-
-    val convexHull = Extremes.fromPoints(points).map { extremes =>
-      val Extremes(minX,maxX,minY,maxY) = extremes
-      expandIfPossible(minX, maxY, points, aboveLine = true) ++
-        expandIfPossible(maxY, maxX, points, aboveLine = true) ++
-        expandIfPossible(maxX, minY, points, aboveLine = false) ++
-        expandIfPossible(minY, minX, points, aboveLine = false)
-    }.getOrElse(Nil)
-
-    println(convexHull)
+    val convexHull = calculateConvexHull(points)
     println(convexHull.map(_.length).sum)
   }
 
+  def calculateConvexHull(points: Seq[Point]): List[LineSegment] = {
+    val (minX,maxX) = points.tail.foldLeft(points.head -> points.head){ case ((minX,maxX),point) =>
+      if(point.x < minX.x) point -> maxX
+      else if (point.x > maxX.x) minX -> point
+      else minX -> maxX
+    }
+    val line = LineSegment(minX,maxX)
+    expandConvexHull(line, points, aboveLine = true) ++ expandConvexHull(line, points, aboveLine = false)
+  }
+
   /**
-    * Given an lineSegment that is part of the convex hull and points in the plane.
-    * It will split the lineSegment in two lines, and tries to expand the convex hull of those lines recursively as well.
+    * Given an line that is part of the convex hull and points in the plane.
+    * It will split the line in two lines, and tries to expand the convex hull of those lines recursively as well.
     * A line is split if there exists a point above/below the line, it will take the point with the maximum distance perpendicular to the line.
     */
-  def expandConvexHull(lineSegment: LineSegment, points: Seq[Point], aboveLine: Boolean): List[LineSegment] = {
-      val epsilon = 0.0002D
-      val line = lineSegment.toLine
-      val remainingPoints = if (aboveLine) points.filter(line.isAbove(epsilon)) else points.filter(line.isBelow(epsilon))
+  private def expandConvexHull(line: LineSegment, points: Seq[Point], aboveLine: Boolean): List[LineSegment] = {
+    val remainingPoints = if (aboveLine) points.filter(line.isAbove) else points.filter(line.isBelow)
 
-       println(s"points remaining $remainingPoints")
-
-      if (remainingPoints.isEmpty) List(lineSegment)
-      else {
-        val furthestPoint = remainingPoints.maxBy(line.perpendicularDistance)
-        val splitLineA = LineSegment(lineSegment.from, furthestPoint)
-        val splitLineB = LineSegment(furthestPoint, lineSegment.to)
-
-         println(s"Going to recursive call line: $splitLineA and $splitLineB with points $remainingPoints and aboveLine=$aboveLine")
-
-        expandConvexHull(splitLineA, remainingPoints, aboveLine) ++ expandConvexHull(splitLineB, remainingPoints, aboveLine)
-      }
+    if (remainingPoints.isEmpty) List(line)
+    else {
+      val furthestPoint = remainingPoints.maxBy(line.perpendicularDistance)
+      val splitLineA = LineSegment(line.from, furthestPoint)
+      val splitLineB = LineSegment(furthestPoint, line.to)
+      expandConvexHull(splitLineA, remainingPoints, aboveLine) ++ expandConvexHull(splitLineB, remainingPoints, aboveLine)
+    }
   }
 }
--- src/test/scala/funprog/recursion/c8/SolutionTest.scala
+++ src/test/scala/funprog/recursion/c8/SolutionTest.scala
@@ -1,51 +1,58 @@
 package funprog.recursion.c8
 
-import funprog.recursion.c8.Solution.{Extremes, LinePartitioning, LineSegment, Point}
+import funprog.recursion.c8.Solution.{LineSegment, Point}
 import org.scalatest.{Matchers, WordSpec}
 
 class SolutionTest extends WordSpec with Matchers {
 
-  "The line's isAbove and isBelow function" should {
+  "The line and lineSegment theirs isAbove and isBelow function" should {
 
     "correctly work with integer points" in {
-      val epsilon = 0.0D
       val points = for(x <- -3 to 3; y <- -1 to 1) yield Point(x,y)
-      val line = LineSegment(Point(-3,-1), Point(3,1)).toLine
+      val lineSegment = LineSegment(Point(-3,-1), Point(3,1))
       val expectedAboveLine = points.filter(point => point.x < point.y*3).toList
       val expectedOnLine =  points.filter(point => point.x == point.y*3).toList
       val expectedBelowLine = points.filter(point => point.x > point.y*3).toList
 
-      points.filter(line.isAbove(epsilon)) should contain theSameElementsAs expectedAboveLine
-      points.filter(point => !line.isAbove(epsilon)(point) && !line.isBelow(epsilon)(point)) should contain theSameElementsAs expectedOnLine
-      points.filter(line.isBelow(epsilon)) should contain theSameElementsAs expectedBelowLine
+      points.filter(lineSegment.isAbove) should contain theSameElementsAs expectedAboveLine
+      points.filter(point => !lineSegment.isAbove(point) && !lineSegment.isBelow(point)) should contain theSameElementsAs expectedOnLine
+      points.filter(lineSegment.isBelow) should contain theSameElementsAs expectedBelowLine
     }
 
     "partition double points correctly" in {
-      val epsilon = 0.000002D
       val generatedPoints = for(x <- -3 to 3; y <- -1 to 1) yield Point(x+0.1*Math.random(),y+0.1*Math.random())
       val onLinePoints = for(i <- -3 to 3) yield Point(i*1.0D, i/3.0D)
-      val line = LineSegment(Point(-3,-1), Point(3,1)).toLine
-      val expectedAboveLine = generatedPoints.filter(point => point.y > (point.x/3 + epsilon)).toList
-      val expectedOnLine =  onLinePoints ++ generatedPoints.filter(point => point.y >= (point.x/3 - epsilon) && point.y <= (point.x/3 + epsilon)).toList
-      val expectedBelowLine = generatedPoints.filter(point => point.y < (point.x/3 - epsilon)).toList
+      val lineSegment = LineSegment(Point(-3,-1), Point(3,1))
+      val expectedAboveLine = generatedPoints.filter(point => point.y > (point.x/3)).toList
+      val expectedOnLine =  onLinePoints ++ generatedPoints.filter(point => point.y >= (point.x/3) && point.y <= (point.x/3)).toList
+      val expectedBelowLine = generatedPoints.filter(point => point.y < (point.x/3)).toList
 
       val points = generatedPoints ++ onLinePoints
-      points.filter(line.isAbove(epsilon))should contain theSameElementsAs expectedAboveLine
-      points.filter(point => !line.isAbove(epsilon)(point) && !line.isBelow(epsilon)(point)) should contain theSameElementsAs expectedOnLine
-      points.filter(line.isBelow(epsilon)) should contain theSameElementsAs expectedBelowLine
+
+      points.filter(lineSegment.isAbove)should contain theSameElementsAs expectedAboveLine
+      points.filter(point => !lineSegment.isAbove(point) && !lineSegment.isBelow(point)) should contain theSameElementsAs expectedOnLine
+      points.filter(lineSegment.isBelow) should contain theSameElementsAs expectedBelowLine
     }
 
   }
 
-  "The Extremes fromPoints function" should {
 
-    "correctly calculate the extremes" in {
-      val points = List(Point(1, 4), Point(2, 1), Point(2, 3), Point(2, 4),
-        Point(2, 5), Point(3, 8), Point(3, 7), Point(3, 6), Point(3, 3), Point(4, 3),
-        Point(4, 5), Point(6, 5))
+  "The points distance method" should {
 
-      val expectedExtremes = Some(Extremes(Point(1,4), Point(6,5), Point(2,1), Point(3,8)))
-      Extremes.fromPoints(points) shouldBe expectedExtremes
+    "return the correct distance between two positive integer points" in {
+      val from = Point(1,4)
+      val to = Point(5,5)
+      val expectedDistance = Math.sqrt(17)
+      Point.distance(from,to) shouldEqual expectedDistance
+      Point.distance(to,from) shouldEqual expectedDistance
+    }
+
+    "return the correct distance between two integer points" in {
+      val from = Point(-3,-8)
+      val to = Point(5,5)
+      val expectedDistance = Math.sqrt(Math.pow(8,2) + Math.pow(13,2))
+      Point.distance(from,to) shouldEqual expectedDistance
+      Point.distance(to,from) shouldEqual expectedDistance
     }
   }
 
